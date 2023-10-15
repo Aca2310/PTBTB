7ba9e7ad968b25523e3f5e5bec3304a11d4740dd
khairin nisa <khairinnisa2310@gmail.com>
2023-10-15 14:09:03 +0700

    First init

--- app/src/main/java/com/example/ptbtb/MainActivity.java
+++ app/src/main/java/com/example/ptbtb/MainActivity.java
@@ -2,13 +2,26 @@ package com.example.ptbtb;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    Button button_getStarted;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        button_getStarted = (Button) findViewById(R.id.button_getStarted);
+
+        button_getStarted.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), Regist.class);
+                startActivity(intent);
+            }
+        });
     }
 }
--- app/src/main/java/com/example/ptbtb/Regist.java
+++ app/src/main/java/com/example/ptbtb/Regist.java
@@ -2,13 +2,26 @@ package com.example.ptbtb;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Regist extends AppCompatActivity {
+    Button button_signup;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_regist);
+        button_signup = (Button) findViewById(R.id.button_signup);
+
+        button_signup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(),Login.class);
+                startActivity(intent);
+            }
+        });
     }
 }
