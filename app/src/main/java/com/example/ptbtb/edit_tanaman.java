b10382bce98b034f11c04cfff60487e4319c28f8
Husna <husnaafiqahy@gmail.com>
2023-10-26 16:05:50 +0700

    update lagi

--- app/src/main/java/com/example/ptbtb/edit_tanaman.java
+++ app/src/main/java/com/example/ptbtb/edit_tanaman.java
@@ -1,14 +1,27 @@
 package com.example.ptbtb;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.AppCompatImageView;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class edit_tanaman extends AppCompatActivity {
+
+    AppCompatImageView button_back;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_edit_tanaman);
+
+        button_back = findViewById(R.id.button_back);
+        button_back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(edit_tanaman.this, desctp.class));
+            }
+        });
     }
 }
