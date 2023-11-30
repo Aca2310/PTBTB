f04514445c9e486fc734df3a0e89e37db92d5727
Mutiara <2111521006_mutiara@student.unand.ac.id>
2023-11-30 10:43:27 +0700

    tmbhn

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -41,7 +41,9 @@ dependencies {
     implementation("com.google.firebase:firebase-database:20.3.0")
     implementation ("com.google.firebase:firebase-auth:21.0.1")// Firebase Authentication
     implementation ("com.google.firebase:firebase-database:20.3.0") // Firebase Realtime Database
-    implementation ("com.google.firebase:firebase-core:20.0.0") // Firebase Core
+    implementation ("com.google.firebase:firebase-core:20.0.0")
+    implementation ("com.google.firebase:firebase-storage:20.3.0")
+    implementation("com.squareup.picasso:picasso:2.71828") // Firebase Core
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
