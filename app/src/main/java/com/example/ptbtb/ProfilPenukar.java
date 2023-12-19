7c2f727d4bc8ec6993c41b37f40e403c5b10f132
Mutiara <2111521006_mutiara@student.unand.ac.id>
2023-12-19 08:17:59 +0700

    ini

---
+++ app/src/main/java/com/example/ptbtb/ProfilPenukar.java
@@ -0,0 +1,28 @@
+package com.example.ptbtb;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+
+public class ProfilPenukar extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_profil_penukar);
+    }
+}
---
+++ app/src/main/java/com/example/ptbtb/list_profilpenukar.java
@@ -0,0 +1,57 @@
+package com.example.ptbtb;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class list_profilpenukar {
+    private String user_id;
+    private String username;
+    private String nama;
+    private String dataTitle;
+    private String dataDetail;
+    private String dataBarter;
+    private String dataImage;
+
+    public list_profilpenukar() {
+
+    }
+
+    public String getDataTitle() {
+        return dataTitle;
+    }
+
+    public String getDataDetail() {
+        return dataDetail;
+    }
+
+    public String getDataBarter() {
+        return dataBarter;
+    }
+
+    public String getDataImage() {
+        return dataImage;
+    }
+
+    public list_profilpenukar(String user_id, String username, String nama, String dataTitle, String dataDetail, String dataBarter, String dataImage) {
+        this.user_id = user_id;
+        this.username = username;
+        this.nama = nama;
+        this.dataTitle = dataTitle;
+        this.dataDetail = dataDetail;
+        this.dataBarter = dataBarter;
+        this.dataImage = dataImage;
+    }
+
+    public String getUser_id() {
+        return user_id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getNama() {
+        return nama;
+    }
+}
