8b27603919e71d32a630cae4680d421153e2c70c
Mutiara <2111521006_mutiara@student.unand.ac.id>
2024-01-07 16:53:42 +0700

    baru

--- app/src/main/java/com/example/ptbtb/rating.java
+++ app/src/main/java/com/example/ptbtb/rating.java
@@ -5,19 +5,25 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.RatingBar;
 import android.widget.Toast;
 
 public class rating extends AppCompatActivity {
     Button button_rating;
     RatingBar ratingBar;
-    float myRating=0;
-
+    float myRating = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_rating);
+
+        // Dapatkan referensi ImageView
+        ImageView imageViewRating = findViewById(R.id.imageViewRating);
+
+        // Setel gambar di ImageView
+        imageViewRating.setImageResource(R.drawable.bplant); // Ganti dengan sumber gambar sesuai kebutuhan
 
         button_rating = findViewById(R.id.button_rating);
         ratingBar = findViewById(R.id.ratingBar);
@@ -25,12 +31,11 @@ public class rating extends AppCompatActivity {
         ratingBar.setOnRatingBarChangeListener(new RatingBar.OnRatingBarChangeListener() {
             @Override
             public void onRatingChanged(RatingBar ratingBar, float v, boolean b) {
-
                 int rating = (int) v;
                 String message = null;
 
                 myRating = ratingBar.getRating();
-                switch (rating){
+                switch (rating) {
                     case 1:
                         message = "Kecewa";
                         break;
@@ -50,10 +55,33 @@ public class rating extends AppCompatActivity {
                 Toast.makeText(rating.this, message, Toast.LENGTH_SHORT).show();
             }
         });
+
         button_rating.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Toast.makeText(rating.this, "Your Rating Is" + myRating, Toast.LENGTH_SHORT).show();
+                String finalMessage;
+                switch ((int) myRating) {
+                    case 1:
+                        finalMessage = "Kecewa";
+                        break;
+                    case 2:
+                        finalMessage = "Kurang Baik";
+                        break;
+                    case 3:
+                        finalMessage = "Biasa";
+                        break;
+                    case 4:
+                        finalMessage = "Baik";
+                        break;
+                    case 5:
+                        finalMessage = "Luar Biasa";
+                        break;
+                    default:
+                        finalMessage = "Tidak Ada Rating";
+                        break;
+                }
+
+                Toast.makeText(rating.this, "Your Rating Is " +  myRating + "\n" + finalMessage, Toast.LENGTH_SHORT).show();
             }
         });
     }
